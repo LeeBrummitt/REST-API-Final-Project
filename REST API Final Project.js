dc101b861b137cf23f82bc7302600232690d2105
Lee Brummitt <leebrummitt@gmail.com>
2018-11-10 00:29:03 +0000

    Added index.html and got it to return a result to console

--- REST API Final Project.js
+++ REST API Final Project.js
@@ -1,3 +1,5 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://omega.unasec.info/";
 const express = require('express');
 const app = express();
 
@@ -8,7 +10,17 @@ app.listen(8080);
 
 //seperate test files needed
 app.get('/server/review/:reviewid', function(req, response) {
-    response.sendfile(__dirname + "/test1.json");
+    
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("amazon");
+        dbo.collection("reviews").findOne({}, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
+    });
+    
 });
 
 app.get('/server/review/:n/:stars', function(req, response) {
